b5b6770bd38e887eab98db2e8e64c2167e494225
Jonas Praast <108411785+jonasp2004@users.noreply.github.com>
2023-07-06 12:41:51 +0200

    Added setup pages

--- Dialogs/Launch.xaml.cs
+++ Dialogs/Launch.xaml.cs
@@ -1,37 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Berichtsheft.Pages.Setup;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace Berichtsheft.Dialogs {
-    /// <summary>
-    /// Interaktionslogik für Launch.xaml
-    /// </summary>
+
     public partial class Launch : Window {
         public Launch() {
             InitializeComponent();
+
+            // For Alpha releases
             if(DateTime.Now.ToFileTimeUtc() > Properties.Settings.Default.expDate.ToFileTimeUtc()) {
-                fr_setup.Content = new Berichtsheft.Pages.Setup.VersionExpiredä();
-                btn_continue.Visibility = Visibility.Collapsed;
+                fr_setup.Content = new VersionExpiredä();
                 btn_next.Visibility = Visibility.Collapsed;
                 btn_back.Visibility = Visibility.Collapsed;
             } else {
-                fr_setup.Content = new Berichtsheft.Pages.Setup.Welcome();
+                if(Properties.Settings.Default.setupDone == false) {
+                    fr_setup.Content = new Berichtsheft.Pages.Setup.Welcome();
+                } else {
+                    SubMainWIndow smw = new SubMainWIndow();
+                    smw.Show();
+                    Close();
+                }
+            }
+
+            // For regular releases
+            //if(Properties.Settings.Default.setupDone == false) {
+            // fr_setup.Content = new Berichtsheft.Pages.Setup.Welcome();
+            //} else {
+            // SubMainWIndow smw = new SubMainWIndow();
+            // smw.Show();
+            // Close();
+            // }
+        }
+
+        private void btn_next_Click(object sender, RoutedEventArgs e) {
+            if(fr_setup.Content.GetType() == typeof(Welcome)) {
+                fr_setup.Content = new Tutorial();
+            } else if(fr_setup.Content.GetType() == typeof(Tutorial)) {
+                btn_closeWindow.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
             }
         }
 
-        private void btn_continue_Click(object sender, RoutedEventArgs e) {
-            SubMainWIndow subMainWIndow = new SubMainWIndow();
-            subMainWIndow.Show();
+        private void btn_back_Click(object sender, RoutedEventArgs e) {
+            if(fr_setup.CanGoBack) {
+                fr_setup.GoBack();
+            }
+        }
+
+        private async void btn_closeWindow_Click(object sender, RoutedEventArgs e) {
+            await System.Threading.Tasks.Task.Delay(100);
             Close();
         }
     }
